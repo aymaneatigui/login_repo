0868b9dede7e175ce77d8a637901b2f809601e7f
Aymane Atigui <aymane.atigui.0202@gmail.com>
2023-03-10 18:05:55 +0100

    Login with google

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,36 +1,48 @@
 import { Link } from "react-router-dom";
-function Nav() {
-    return (
-        // <div>
-        //     <Link to="/">Home</Link>
-        //     <Link to="/login">Login</Link>
-        // </div>
-        <nav className="bg-gray-900 shadow-lg">
-      <div className="mx-auto px-4 py-2 max-w-7xl flex justify-between items-center">
-        <div className="flex items-center">
-          <Link to="/" className="text-white font-bold text-lg mr-4">
-            Home
-          </Link>
-        </div>
-        <div className="flex items-center">
-          <Link to="/login" className="text-gray-400 hover:text-white mr-4">
-            Login
-          </Link>
-          <button className="text-gray-400 hover:text-white focus:outline-none mr-4">
-            Logout
-          </button>
-          <div className="ml-2">
-            <img
-              src="https://via.placeholder.com/40"
-              alt="Profile"
-              className="rounded-full m-auto"
-            />
-            <p className="text-gray-200 text-sm mt-1">Aymane Atigui</p>
-          </div>
+import { auth } from "../config/fairebase";
+import {useAuthState} from "react-firebase-hooks/auth"
+import { signOut } from "firebase/auth";
 
-        </div>
-      </div>
-    </nav>
+function Nav() {
+
+    const [user] = useAuthState(auth);
+
+    const signout = async ()=>{
+        await signOut(auth);
+    }
+
+    return (
+        <nav className="bg-gray-900 shadow-lg">
+            <div className="mx-auto px-4 py-2 max-w-7xl flex justify-between items-center">
+                <div className="flex items-center">
+                    <Link to="/" className="text-white font-bold text-lg mr-4">
+                        Home
+                    </Link>
+                </div>
+                <div className="flex items-center">
+                    {!user && (
+                    <Link to="/login" className="text-gray-400 hover:text-white mr-4">
+                        Login
+                    </Link>
+                    )}
+                    {user && (
+                        <>
+                    <button onClick={signout}
+                        className="text-gray-400 hover:text-white focus:outline-none mr-4">
+                        Logout
+                    </button>
+                    <div className="ml-2">
+                        <img src={user?.photoURL} alt="Profile" className="rounded-full m-auto" width="50" height="50"/>
+                        <p className="text-gray-200 text-sm mt-1">
+                            {user?.displayName}
+                        </p>
+                    </div>
+                    </>
+                    )}
+
+                </div>
+            </div>
+        </nav>
     );
 }
 
